771736036721c90068e6a967792f4ef44ba032ec
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-08-16 21:49:20 -0300

    service de acao

---
+++ web/jusaway-web/src/app/cadastrobasico/acao/acao.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Acao } from './acao';
+
+@Injectable()
+export class AcaoService {
+
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+    private acoesUrl = 'acoes';  // URL to web api
+
+    constructor(private http: Http) { }
+
+    getAcoes(): Observable<Acao[]> {
+        return this.http.get(this.acoesUrl)
+            .map(res => res.json());
+    }
+
+}
